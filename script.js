ba93fed376fc63549581334f7d51eb5181908515
lacerdagb <127674463+lacerdagb@users.noreply.github.com>
2024-01-06 14:41:02 -0300

    Fiz o esqueleto no JS, e fiz a função alerta

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("TESTE")
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document. querySelector ("#button");
+
+let sizePassword = document.querySelector ("#valor");
+let password = document.querySelector ("#password");
+
+let containerPassword = document.querySelector ("#container-input");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value; /* Altera o script do valor do tamanho */
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    alert ("VAMOS CRIAR A SENHA")
+}

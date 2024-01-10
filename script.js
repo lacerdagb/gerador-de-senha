a9bdb638ba5cb46bd7ef2f166054061d8bb21f88
lacerdagb <127674463+lacerdagb@users.noreply.github.com>
2024-01-09 21:43:19 -0300

    JS funcionaldiade dos números aleatórios, não está sendo exibido na tela

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let password = document.querySelector ("#password");
 
 let containerPassword = document.querySelector ("#container-input");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value; /* Altera o script do valor do tamanho */
@@ -16,6 +16,15 @@ slider.oninput = function() {
     sizePassword.innerHTML = this.value;
 }
 
-function generatePassword() {
-    alert ("VAMOS CRIAR A SENHA")
+function generatePassword() { //Função para gerar os números aleatórios
+
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+       }
+     console.log(pass); //Esse comando mostra o resultado no Inspecionar > Console 
+
+    containerPassword.classList.remove("hide"); // Vai remover o hide para mostrar a senha.
+    password.innerHTML = pass;
+
 }
